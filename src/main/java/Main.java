35d97ff22c54b148f169b8941ccd506ee408b87b
Md Farid <mdfarid.0118@gmail.com>
2026-01-10 00:30:31 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,20 +25,20 @@ public class Main {
         byte[] bufResponse = new byte[512];
         int offset = 0;
 
-        // ===== DNS HEADER (12 bytes) =====
-        setPacketId(bufResponse, 1234); // ID
+        // ===== HEADER =====
+        setPacketId(bufResponse, 1234);
         offset += 2;
 
-        setQR(bufResponse); // Flags (QR = 1)
+        setQR(bufResponse);
         offset += 2;
 
         // QDCOUNT = 1
         bufResponse[offset++] = 0x00;
         bufResponse[offset++] = 0x01;
 
-        // ANCOUNT = 0
+        // ANCOUNT = 1  ✅ NEW
         bufResponse[offset++] = 0x00;
-        bufResponse[offset++] = 0x00;
+        bufResponse[offset++] = 0x01;
 
         // NSCOUNT = 0
         bufResponse[offset++] = 0x00;
@@ -51,34 +51,67 @@ public class Main {
         // ===== QUESTION SECTION =====
 
         // QNAME: codecrafters.io
-        bufResponse[offset++] = 0x0c; // length of "codecrafters"
+        bufResponse[offset++] = 0x0c; // "codecrafters"
         byte[] label1 = "codecrafters".getBytes();
         System.arraycopy(label1, 0, bufResponse, offset, label1.length);
         offset += label1.length;
 
-        bufResponse[offset++] = 0x02; // length of "io"
+        bufResponse[offset++] = 0x02; // "io"
         byte[] label2 = "io".getBytes();
         System.arraycopy(label2, 0, bufResponse, offset, label2.length);
         offset += label2.length;
 
-        bufResponse[offset++] = 0x00; // null terminator
+        bufResponse[offset++] = 0x00; // terminator
 
-        // QTYPE = 1 (A record)
+        // QTYPE = A (1)
         bufResponse[offset++] = 0x00;
         bufResponse[offset++] = 0x01;
 
-        // QCLASS = 1 (IN)
+        // QCLASS = IN (1)
         bufResponse[offset++] = 0x00;
         bufResponse[offset++] = 0x01;
 
-        // Send only the used bytes
-        DatagramPacket packetResponse = new DatagramPacket(
-            bufResponse,
-            offset,
-            packet.getSocketAddress()
-        );
+        // ===== ANSWER SECTION (NEW) =====
 
-        serverSocket.send(packetResponse);
+        // NAME: codecrafters.io
+        bufResponse[offset++] = 0x0c;
+        System.arraycopy(label1, 0, bufResponse, offset, label1.length);
+        offset += label1.length;
+
+        bufResponse[offset++] = 0x02;
+        System.arraycopy(label2, 0, bufResponse, offset, label2.length);
+        offset += label2.length;
+
+        bufResponse[offset++] = 0x00;
+
+        // TYPE = A (1)
+        bufResponse[offset++] = 0x00;
+        bufResponse[offset++] = 0x01;
+
+        // CLASS = IN (1)
+        bufResponse[offset++] = 0x00;
+        bufResponse[offset++] = 0x01;
+
+        // TTL = 60 seconds
+        bufResponse[offset++] = 0x00;
+        bufResponse[offset++] = 0x00;
+        bufResponse[offset++] = 0x00;
+        bufResponse[offset++] = 0x3c;
+
+        // RDLENGTH = 4
+        bufResponse[offset++] = 0x00;
+        bufResponse[offset++] = 0x04;
+
+        // RDATA = 8.8.8.8
+        bufResponse[offset++] = 0x08;
+        bufResponse[offset++] = 0x08;
+        bufResponse[offset++] = 0x08;
+        bufResponse[offset++] = 0x08;
+
+        DatagramPacket responsePacket =
+            new DatagramPacket(bufResponse, offset, packet.getSocketAddress());
+
+        serverSocket.send(responsePacket);
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
